b54af5d53d57bebd4a2ad3fc37b04655c00dcac9
minyeoungKim <to__get___her@naver.com>
2019-01-25 13:02:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -148,3 +148,12 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 // socket.io 서버를 시작합니다.
 var io = socketio.listen(server);
 console.log('socket.io 요청을 받아들일 준비가 되었습니다./');
+
+// 클라이언트가 연결했을 때의 이벤트 처리
+io.sockets.on('connection',function(socket){
+    console.log('connection info : ', socket.request.connection._peername);
+    
+    //소켓 객체에 클라이언트 Host, Port 정보 속성으로 추가
+    socket.remoteAddress = socket.request.connection._peername.address;
+    socket.remotePort = socket.request.connection._peername.port;
+});
